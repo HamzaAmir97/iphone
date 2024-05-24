2dd2fbc61c71779bac45b4fbd0f1fac18b8b84dd
Hmazah <alhamza2012@gmail.com>
2024-05-24 10:00:00 +0300

    add gsap repo

--- src/components/vedioCarousel.jsx
+++ src/components/vedioCarousel.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import { highlightslides } from '../utils'
+import {hightlightsSlides} from '../constants'
 
 const VideoCarousel = () => {
   return (
     <div className="flex items-center">
       VideoCarousel
-      {highlightslides.map((list, index) => (
+      {hightlightsSlides.map((list, index) => (
         <div key={list.id} id="slider" className="sm:pr-20 pr-10">   
           <div className="video-carousel_container">
-            
-          <div className="video-carousel_item">
-            <div className="video-carousel_item_img">
-              <img src={list.image} alt={list.title} />
-            </div>
+
+          <div className="w-full h-full flex-center
+            bg-black rounded-3xl overflow-hidden ">
+           <video >
+            <source src={list.video} type="video/mp4" />
+         
+         
+          </video>
+
+
           </div>
                 
           </div>
