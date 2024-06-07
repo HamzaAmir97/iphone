35de462dda3ee5d0b47766f8acefa94382a99bd6
Hmazah <alhamza2012@gmail.com>
2024-06-07 10:00:00 +0300

    add three

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,93 +1,86 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-import { ModelView } from "./ModelView";
-import {yellowImg} from "../utils";
-import { useState, useRef } from "react";
-import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
-import { View } from "@react-three/drei";
+// import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
+import { yellowImg } from "../utils";
+
+import * as THREE from 'three';
+// import { Canvas } from "@react-three/fiber";
+// import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
-
-
-
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
+  const [size, setSize] = useState('small');
+  const [model, setModel] = useState({
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg,
+  })
 
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
 
-const [size, setSize] = useState("small");
-const [model, setModel] = useState({
-    title: "iphone 15 pro in Natural Titanum",
-    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
-    image: yellowImg,
+  // model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
 
-});
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
 
-// camera control for model view
-  
-const cameraControlSmall = useRef();
-const cameraControlLarge = useRef();
+  const tl = gsap.timeline();
 
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
 
-//model v
-const small = useRef(new THREE.Group());
-const large = useRef(new THREE.Group());
-
-
-//rotation
-const [smallRotation, setSmallRotation] = useState(0);
-const [largeRotation, setLargeRotation] = useState(0);
-
-
-
-
-
-
-
-
-
+    if(size ==='small') {
+      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
 
   useGSAP(() => {
-  gsap.to('#heading', {
-         
-         opacity: 1, y: 0, delay: 0.5,
-          
-      })
-
-        }, [])
-
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
 
   return (
-    <section className='common-padding'>
-    <div className='screen-max-width'>
-    
-        <h1 id='heading' className='section-heading'>
-         Take colser look.
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
         </h1>
-        <div className='flex flex-col  items-center mt-5'>
-        <div className=' w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
 
-               <ModelView 
-               index={1}
-                gruopRef={small}
-                gropuType="view1"
-                controlRef={cameraControlSmall}
-                setRotationState={setSmallRotation}
-                item={Model}
-                size={size}
-               />
-              <ModelView 
-               index={2}
-                gruopRef={large}
-                gropuType="view2"
-                controlRef={cameraControlLarge}
-                setRotationState={setLargeRotation}
-                item={Model}
-                size={size}
-               />
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            {/* <ModelView 
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />  
 
-              
+            <ModelView 
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            /> */}
+{/* 
             <Canvas
               className="w-full h-full"
               style={{
@@ -101,23 +94,18 @@ const [largeRotation, setLargeRotation] = useState(0);
               eventSource={document.getElementById('root')}
             >
               <View.Port />
-            </Canvas>
+            </Canvas> */}
+          </div>
 
-        <div className="mx-auto w-ful">
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
 
-         <p className="text-sm font-light
-         text-center mb-5">{model.title}</p>
-        <div className="flex-center">
-
-        <ul className="color-container">
+            <div className="flex-center">
+              <ul className="color-container">
                 {models.map((item, i) => (
-                  <li key={i} className="w-6 h-6 rounded-full 
-                  mx-2 cursor-pointer" 
-                  style={{ backgroundColor: item.color[0] }} 
-                  onClick={() => setModel(item)} />
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
                 ))}
               </ul>
-
 
               <button className="size-btn-container">
                 {sizes.map(({ label, value }) => (
@@ -126,30 +114,12 @@ const [largeRotation, setLargeRotation] = useState(0);
                   </span>
                 ))}
               </button>
-
+            </div>
+          </div>
         </div>
-
-
-
-
-        </div>
-        </div>
-        
-        
-        
-        
-        
-        
-        
-        
-        </div>
-        
-        </div>
-
-
-
+      </div>
     </section>
   )
 }
 
-export default Model;
+export default Model
