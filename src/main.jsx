39472a0c51c45e3cf46a99446d7df692a3e40858
Hmazah <alhamza2012@gmail.com>
2024-06-21 10:00:00 +0300

    add  iphone scale

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//...
+//...s
 import * as Sentry from "@sentry/react";
 
-import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://fb18e849208f03391cfba9c0d17fa8c0@o4509357732397056.ingest.de.sentry.io/4509357790068816",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  integrations: [
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect
+    })
+  ],
   sendDefaultPii: true
 });
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<App />);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
